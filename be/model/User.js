79ee09cc81690895184ded3c3109dfce720906fe
rsoewandi <soewandi346@gmail.com>
2024-12-12 16:06:27 +0700

    Update User.js

--- be/model/User.js
+++ be/model/User.js
@@ -1,16 +1,19 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db'); // Import koneksi database
+const sequelize = require('../config/db'); 
 
 const User = sequelize.define('User', {
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true, // Username harus unik
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
+  // Kolom untuk Username
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,  // Username harus unik
+  },
+  // Kolom untuk Password
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,  // Password tidak boleh kosong
+  },
 });
 
+// Export model User
 module.exports = User;
